9e0c360ae7be5983f87cdefaa654553784eb84d0
Tamir <1tamir198@gmail.com>
2022-11-28 18:15:51 +0200

    Adding event listeners to all the buttons

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
@@ -13,7 +12,7 @@ import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     //-1 is no player 1 is x 0 is O
-    static int[] cells = {-1, -1, -1, -1, -1, -1, -1, -1, -1};
+    static int[] cells = new int[]{-1, -1, -1, -1, -1, -1, -1, -1, -1};
     // 1 = X, 0 = o
     static int currentPlayer = 1;
     static int stepsCounter = 0;
@@ -22,8 +21,8 @@ public class MainActivity extends AppCompatActivity {
         return currentPlayer;
     }
 
-    public void setCurrentPlayer(int currentPlayer) {
-        this.currentPlayer = currentPlayer;
+    public static void changeCurrentPlayer() {
+        currentPlayer = (currentPlayer == 1) ? 0 : 1;
     }
 
     public static void resetGame() {
@@ -34,55 +33,51 @@ public class MainActivity extends AppCompatActivity {
     public static boolean isWon() {
         int winner;
         return (cells[0] + cells[1] + cells[2] == 0 || cells[0] + cells[1] + cells[2] == 3 ||
-        cells[3] + cells[4] + cells[5] == 0 || cells[3] + cells[4] + cells[5] == 3 ||
-        cells[6] + cells[7] + cells[8] == 0 || cells[6] + cells[7] + cells[8] == 3 ||
-        cells[0] + cells[3] + cells[6] == 0 || cells[0] + cells[3] + cells[6] == 3 ||
-        cells[1] + cells[4] + cells[7] == 0 || cells[1] + cells[4] + cells[7] == 3 ||
-        cells[2] + cells[5] + cells[8] == 0 || cells[2] + cells[5] + cells[8] == 3 ||
-        cells[0] + cells[4] + cells[8] == 0 || cells[0] + cells[4] + cells[8] == 3 ||
-        cells[2] + cells[4] + cells[6] == 0 || cells[2] + cells[4] + cells[6] == 3);
+                cells[3] + cells[4] + cells[5] == 0 || cells[3] + cells[4] + cells[5] == 3 ||
+                cells[6] + cells[7] + cells[8] == 0 || cells[6] + cells[7] + cells[8] == 3 ||
+                cells[0] + cells[3] + cells[6] == 0 || cells[0] + cells[3] + cells[6] == 3 ||
+                cells[1] + cells[4] + cells[7] == 0 || cells[1] + cells[4] + cells[7] == 3 ||
+                cells[2] + cells[5] + cells[8] == 0 || cells[2] + cells[5] + cells[8] == 3 ||
+                cells[0] + cells[4] + cells[8] == 0 || cells[0] + cells[4] + cells[8] == 3 ||
+                cells[2] + cells[4] + cells[6] == 0 || cells[2] + cells[4] + cells[6] == 3);
     }
 
-//    public static void addButtonListener(int i){
-//
-//            cells[i] = currentPlayer;
-//            stepsCounter++;
-//            if(currentPlayer == 1) {
-//                btn1.setBackgroundResource(R.drawable.x);
-//                if(isWon()){
-//                    playersText.setImageResource(R.drawable.xwin);
-//                }
-//            }
-//            else {
-//                btn1.setBackgroundResource(R.drawable.o);
-//                if(isWon()){
-//                    playersText.setImageResource(R.drawable.owin);
-//                }
-//            }
-//
-//            // out of steps and no one won
-//            if(stepsCounter == 9){
-//                if(!isWon()){
-//                    playersText.setImageResource(R.drawable.nowin);
-//                }
-//            }
-//
-//            btn1.setEnabled(false);
-//
-//
-//    }
+    public static void addButtonListener(int i, Button btn, ImageView playersText) {
+        cells[i] = currentPlayer;
+        stepsCounter++;
+        if (currentPlayer == 1) {
+            btn.setBackgroundResource(R.drawable.x);
+            if (isWon()) {
+                Log.i("IS winning", "X is winning");
+                playersText.setImageResource(R.drawable.xwin);
+            }
+        } else {
+            btn.setBackgroundResource(R.drawable.o);
+            if (isWon()) {
+                Log.i("IS winning", "O is winning");
+
+                playersText.setImageResource(R.drawable.owin);
+            }
+        }
+
+        // out of steps and no one won
+        if (stepsCounter == 9) {
+            if (!isWon()) {
+                playersText.setImageResource(R.drawable.nowin);
+            }
+        }
+
+        btn.setEnabled(false);
+        //TODO check why calling this here is making the game as won every turn
+//        changeCurrentPlayer();
+        Log.i("This is a test", Arrays.toString(cells));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("This is a test ", String.valueOf(isWon()));
-        //TODO danielle Add event listners here
-        //Give id for each button, when you click on the button :
-        // change the cells array
-        //call isWon on click
-        //is the result of isWon is + finish the game bla bla
-        // check isWon
+
 
         Button btn1 = findViewById(R.id.btn1);
         Button btn2 = findViewById(R.id.btn2);
@@ -96,90 +91,14 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView playersText = findViewById(R.id.playerText);
 
-
-
-        btn1.setOnClickListener(view ->{
-            cells[1] = currentPlayer;
-            stepsCounter++;
-            if(currentPlayer == 1) {
-                btn1.setBackgroundResource(R.drawable.x);
-                if(isWon()){
-                    playersText.setImageResource(R.drawable.xwin);
-                }
-            }
-            else {
-                btn1.setBackgroundResource(R.drawable.o);
-                if(isWon()){
-                    playersText.setImageResource(R.drawable.owin);
-                }
-            }
-
-            // out of steps and no one won
-            if(stepsCounter == 9){
-                if(!isWon()){
-                    playersText.setImageResource(R.drawable.nowin);
-                }
-            }
-
-            btn1.setEnabled(false);
-
-        });
-
-        btn2.setOnClickListener(view ->{
-            cells[2] = currentPlayer;
-            stepsCounter++;
-            if(currentPlayer == 1) {
-                btn2.setBackgroundResource(R.drawable.x);
-                if(isWon()){
-                    playersText.setImageResource(R.drawable.xwin);
-                }
-            }
-            else {
-                btn2.setBackgroundResource(R.drawable.o);
-                if(isWon()){
-                    playersText.setImageResource(R.drawable.owin);
-                }
-            }
-
-            // out of steps and no one won
-            if(stepsCounter == 9){
-                if(!isWon()){
-                    playersText.setImageResource(R.drawable.nowin);
-                }
-            }
-
-            btn2.setEnabled(false);
-
-        });
-
-        btn3.setOnClickListener(view ->{
-            cells[3] = currentPlayer;
-            stepsCounter++;
-            if(currentPlayer == 1) {
-                btn3.setBackgroundResource(R.drawable.x);
-                if(isWon()){
-                    playersText.setImageResource(R.drawable.xwin);
-                }
-            }
-            else {
-                btn3.setBackgroundResource(R.drawable.o);
-                if(isWon()){
-                    playersText.setImageResource(R.drawable.owin);
-                }
-            }
-
-            // out of steps and no one won
-            if(stepsCounter == 9){
-                if(!isWon()){
-                    playersText.setImageResource(R.drawable.nowin);
-                }
-            }
-
-            btn3.setEnabled(false);
-
-        });
-
-
-
+        btn1.setOnClickListener(view -> addButtonListener(0, btn1, playersText));
+        btn2.setOnClickListener(view -> addButtonListener(1, btn2, playersText));
+        btn3.setOnClickListener(view -> addButtonListener(2, btn3, playersText));
+        btn4.setOnClickListener(view -> addButtonListener(3, btn4, playersText));
+        btn5.setOnClickListener(view -> addButtonListener(4, btn5, playersText));
+        btn6.setOnClickListener(view -> addButtonListener(5, btn6, playersText));
+        btn7.setOnClickListener(view -> addButtonListener(6, btn7, playersText));
+        btn8.setOnClickListener(view -> addButtonListener(7, btn8, playersText));
+        btn9.setOnClickListener(view -> addButtonListener(8, btn9, playersText));
     }
 }
