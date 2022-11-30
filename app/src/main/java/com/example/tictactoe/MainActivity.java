387337961173b9b60b84948456173efc8e40cc89
Tamir <1tamir198@gmail.com>
2022-11-30 13:33:14 +0200

    Draw a line when game is finished

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -18,6 +18,17 @@ public class MainActivity extends AppCompatActivity {
     // 1 = X, 0 = o
     static int currentPlayer = 1;
     static int stepsCounter = 0;
+    Button btn1;
+    Button btn2;
+    Button btn3;
+    Button btn4;
+    Button btn5;
+    Button btn6;
+    Button btn7;
+    Button btn8;
+    Button btn9;
+
+    static ImageView lineImg;
 
     public static void changeCurrentPlayer(ImageView playersText) {
         if (currentPlayer == 1) {
@@ -44,6 +55,10 @@ public class MainActivity extends AppCompatActivity {
         playAgain.setVisibility(View.INVISIBLE);
         playAgain.setEnabled(false);
 
+        lineImg.setRotation(0);
+        lineImg.setVisibility(View.GONE);
+
+
     }
 
     public void displayPlayAgainBtn() {
@@ -53,16 +68,61 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public static boolean isWon() {
-        int winner;
-        return (cells[0] + cells[1] + cells[2] == 0 || cells[0] + cells[1] + cells[2] == 3 ||
-                cells[3] + cells[4] + cells[5] == 0 || cells[3] + cells[4] + cells[5] == 3 ||
-                cells[6] + cells[7] + cells[8] == 0 || cells[6] + cells[7] + cells[8] == 3 ||
-                cells[0] + cells[3] + cells[6] == 0 || cells[0] + cells[3] + cells[6] == 3 ||
-                cells[1] + cells[4] + cells[7] == 0 || cells[1] + cells[4] + cells[7] == 3 ||
-                cells[2] + cells[5] + cells[8] == 0 || cells[2] + cells[5] + cells[8] == 3 ||
-                cells[0] + cells[4] + cells[8] == 0 || cells[0] + cells[4] + cells[8] == 3 ||
-                cells[2] + cells[4] + cells[6] == 0 || cells[2] + cells[4] + cells[6] == 3);
+    public boolean isWon() {
+        boolean topRow = cells[0] + cells[1] + cells[2] == 0 || cells[0] + cells[1] + cells[2] == 3;
+        boolean midRow = cells[3] + cells[4] + cells[5] == 0 || cells[3] + cells[4] + cells[5] == 3;
+        boolean bottomRow = cells[6] + cells[7] + cells[8] == 0 || cells[6] + cells[7] + cells[8] == 3;
+
+        boolean leftColumn = cells[0] + cells[3] + cells[6] == 0 || cells[0] + cells[3] + cells[6] == 3;
+        boolean midColumn = cells[1] + cells[4] + cells[7] == 0 || cells[1] + cells[4] + cells[7] == 3;
+        boolean rightColumn = cells[2] + cells[5] + cells[8] == 0 || cells[2] + cells[5] + cells[8] == 3;
+
+        boolean topLeftBottomRight = cells[0] + cells[4] + cells[8] == 0 || cells[0] + cells[4] + cells[8] == 3;
+        boolean topRightBottomLeft = cells[2] + cells[4] + cells[6] == 0 || cells[2] + cells[4] + cells[6] == 3;
+
+
+        if (topRow) {
+            lineImg.setImageResource(R.drawable.mark3);
+            lineImg.setRotation(90);
+            lineImg.setVisibility(View.VISIBLE);
+        } else if (midRow) {
+            lineImg.setImageResource(R.drawable.mark4);
+            lineImg.setRotation(90);
+            lineImg.setVisibility(View.VISIBLE);
+
+        } else if (bottomRow) {
+            lineImg.setImageResource(R.drawable.mark5);
+            lineImg.setRotation(90);
+            lineImg.setVisibility(View.VISIBLE);
+
+        } else if (leftColumn) {
+            lineImg.setImageResource(R.drawable.mark3);
+            lineImg.setVisibility(View.VISIBLE);
+
+        } else if (midColumn) {
+            lineImg.setImageResource(R.drawable.mark4);
+            lineImg.setVisibility(View.VISIBLE);
+
+
+        } else if (rightColumn) {
+            lineImg.setImageResource(R.drawable.mark5);
+            lineImg.setVisibility(View.VISIBLE);
+
+
+        } else if (topLeftBottomRight) {
+            lineImg.setImageResource(R.drawable.mark1);
+            lineImg.setVisibility(View.VISIBLE);
+
+
+        } else if (topRightBottomLeft) {
+            lineImg.setImageResource(R.drawable.mark2);
+            lineImg.setVisibility(View.VISIBLE);
+        }
+
+
+        return (topRow || midRow || bottomRow ||
+                leftColumn || midColumn || rightColumn ||
+                topLeftBottomRight || topRightBottomLeft);
     }
 
     public void addButtonListener(int i, Button btn, ImageView playersText) {
@@ -110,17 +170,18 @@ public class MainActivity extends AppCompatActivity {
         playAgain.setVisibility(View.INVISIBLE);
         playAgain.setEnabled(false);
 
-        Button btn1 = findViewById(R.id.btn1);
-        Button btn2 = findViewById(R.id.btn2);
-        Button btn3 = findViewById(R.id.btn3);
-        Button btn4 = findViewById(R.id.btn4);
-        Button btn5 = findViewById(R.id.btn5);
-        Button btn6 = findViewById(R.id.btn6);
-        Button btn7 = findViewById(R.id.btn7);
-        Button btn8 = findViewById(R.id.btn8);
-        Button btn9 = findViewById(R.id.btn9);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
 
         ImageView playersText = findViewById(R.id.playerText);
+        lineImg = findViewById(R.id.lineImage);
 
         btn1.setOnClickListener(view -> addButtonListener(0, btn1, playersText));
         btn2.setOnClickListener(view -> addButtonListener(1, btn2, playersText));
@@ -132,7 +193,7 @@ public class MainActivity extends AppCompatActivity {
         btn8.setOnClickListener(view -> addButtonListener(7, btn8, playersText));
         btn9.setOnClickListener(view -> addButtonListener(8, btn9, playersText));
 
-        playAgain.setOnClickListener(view -> resetGame(playersText,playAgain,
+        playAgain.setOnClickListener(view -> resetGame(playersText, playAgain,
                 new Button[]{btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9})
         );
 
